7a63b4129f77c8db75ea932ac07e5eadbb993724
Jacopo Marzola <marzola.jacopo@gmail.com>
2023-06-23 17:48:57 +0200

    done

--- index.js
+++ index.js
@@ -1,23 +1,63 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://127.0.0.1:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Ragù",
+      level: "Amateur Chef",
+      ingredients: [
+        "onions",
+        "carrots",
+        "celeries",
+        "meat",
+        "tomatos",
+        "salt",
+        "pepper",
+        "oil",
+      ],
+      cusine: "italian",
+      dishType: "main_course",
+      image:
+        "https://cdn.cook.stbm.it/thumbnails/ricette/142/142519/hd750x421.jpg",
+      duration: 90,
+      creator: "Frate Indovino",
+    });
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((result) => {
+    console.log("new recipe created", result);
+    return Recipe.insertMany(data);
+  })
+  .then((result) => {
+    console.log("succes, 5 recipes added", result);
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { $set: { duration: 100 } },
+      { new: true }
+    );
+  })
+  .then((result) => {
+    console.log("recipe updated", result);
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((result) => {
+    console.log("recipe deleted", result);
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
+  })
+  .finally(() => {
+    mongoose.connection.close();
   });
